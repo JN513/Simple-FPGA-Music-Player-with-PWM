f4b6529267baaf3057be3fe08cde33348a60a4ea
Julio Nunes Avelar <jn5.guape@gmail.com>
2024-07-31 23:28:54 -0300

    Criando modulo pwm

--- src/pwm.v
+++ src/pwm.v
@@ -1,11 +1,35 @@
-module PWM #(
-    parameter CLK_FREQ = 100000000
-) (
+module PWM (
     input wire clk,
-    input wire rst,
-    input wire [15:0] duty_cycle, // porcentagem do nivel alto da onda
-    input wire [15:0] period, // periodo da onda
-    output wire pwm
+    input wire reset,
+    input wire [20:0] duty_cycle, // duty cycle = period * duty_cycle / 2097152
+    input wire [20:0] period, // clk_freq / pwm_freq = period
+    output reg pwm_out
 );
+
+reg [31:0] counter;
+
+initial begin
+    counter = 0;
+    pwm_out = 0;
+end
+
+always @(posedge clk) begin
+    if (reset) begin
+        counter <= 0;
+        pwm_out <= 0;
+    end else begin
+        if(counter < period) begin
+            counter <= counter + 1'b1;
+        end else begin
+            counter <= 0;
+        end
+    end
+
+    if (counter < duty_cycle) begin
+        pwm_out <= 1'b1;
+    end else begin
+        pwm_out <= 1'b0;
+    end
+end
     
 endmodule
